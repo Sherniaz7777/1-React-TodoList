ab0335349a1a1134a24e5e3a79905e6677febce2
Sherniaz7777 <sherniazibragimov10@gmail.com>
2024-02-08 21:01:18 +0600

    created react

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import TodoListitem from '../todoListitem/TodoListitem';
+import './TodoList.css'
 
 const TodoList = (props) => {
   const  Delete  = props.Delete;
+  const ImpTodo = props.ImpTodo
+  const DoneTodo = props.DoneTodo
+
+  if (props.tododata.length === 0) {
+    return <p>No definite</p>;
+  }
 
   const elements = props.tododata.map(el => (
     <li className='list-group-item' key={el.id}>
-      <TodoListitem title={el.title}  Delete={() => Delete(el.id)} />
+      <TodoListitem title={el.title}  Delete={() => Delete(el.id)}  ImpTodo={() => ImpTodo(el.id)} important={el.important} DoneTodo={() => DoneTodo(el.id)}  done={el.done} allTodo={props.tododata}/>
     </li>
   ));
 
@@ -18,3 +25,4 @@ const TodoList = (props) => {
 }
 
 export default TodoList;
+
